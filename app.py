ade863af43e046e7021677901206e3991378035a
Fentry Martin <fmartin7@emich.edu>
2020-11-02 02:43:07 +0000

    Updated .gitignore app.py

--- app.py
+++ app.py
@@ -15,14 +15,11 @@ def handle_slash():
 @app.route("/test", strict_slashes=False)
 def handle_test():
     input = "ac"
-    return test_module.test(input)
+    return test_module.test(input) 
 
 @app.route("/query", strict_slashes=False)
-def handle_query():
+def handle_paging_query():
     query_term = request.args.get("q")
     query_page = int(request.args.get("p"))
     return jsonify({"query_term": query_term, "search_results": query_on_whoosh.query(query_term, 10, query_page)})
     
-    #search_page = int(request.args.get("p"))
-   # search_results, num_hits, num_pages = query_on_whoosh.performQuery(search_term, search_page, 10)
-    #return jsonify({"query_term": search_term, "num_hits": num_hits, "num_pages": num_pages, "search_results": search_results})
